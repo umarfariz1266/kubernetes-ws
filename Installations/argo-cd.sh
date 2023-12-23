7e8b8dfb62553b249c2d2050bfd7cace306b8446
umarfariz1266 <fariz@provility.com>
2023-12-23 12:58:57 +0530

    corrected

---
+++ Installations/argo-cd.sh
@@ -0,0 +1,4 @@
+kubectl create namespace argocd
+kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
+kubectl patch svc argocd-server -n argocd -p '{"spec": {"type": "NodePort"}}'
+kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d
